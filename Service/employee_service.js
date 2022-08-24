2031b4a77d1d8dbf0fce3ec0afbb2bbb5fd48fdb
ChloeYeung <106992258+ChloeYeung@users.noreply.github.com>
2022-08-24 13:24:08 +0800

    edit employee route and service

--- Service/employee_service.js
+++ Service/employee_service.js
@@ -1,14 +1,47 @@
 class nodeServiceEmployee {
-    constructor(fs, knex) {
-        this.fs = fs;
+    constructor(knex) {
         this.init();
-        this.notes = {};
         this.knex = knex;
     }
     init() {
         new Promise((resolve, reject) => {
 
-        })
+        });
+    }
+    show(user) {
+        return this.knex("users")
+            .select("notes.content", "notes.id")
+            .join("notes", "users.id", "notes.user_id")
+            .where("username", user);
+    }
+
+    add(content, user) {
+        return this.knex("users")
+            .select("id")
+            .where("username", user)
+            .first()
+            .then((data) => {
+                return this.knex("notes").insert({
+                    user_id: data.id,
+                    content: content,
+                });
+            });
+    }
+
+    update(index, content, user) {
+        return this.knex("users")
+            .select("id")
+            .where("username", user)
+            .first()
+            .then(() => {
+                return this.knex("notes").where("id", index).update({
+                    content: content,
+                });
+            });
+    }
+
+    remove(index) {
+        return this.knex("notes").where("id", index).del();
     }
 }
 
