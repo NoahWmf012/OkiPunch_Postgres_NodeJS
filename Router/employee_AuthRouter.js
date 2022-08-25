7d69af18d0bfeed6bb18ca875d6f438022a1d3bb
Chris WU <wumanfung1@gmail.com>
2022-08-25 23:12:13 +0800

    updated employee login but not yet finish redirect page

---
+++ Router/employee_AuthRouter.js
@@ -0,0 +1,47 @@
+class AuthRouter {
+    constructor(express, passport) {
+        this.express = express;
+        this.passport = passport;
+    }
+
+    isLogged(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect("/employee_login");
+    }
+
+    isNotLogged(req, res, next) {
+        if (!req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect("/");
+    }
+
+    router() {
+        let router = this.express.Router();
+
+        router.post(
+            "/employee_login",
+            this.isNotLogged,
+            this.passport.authenticate("local-login", {
+                successRedirect: "/salary/:id", //not yet assign /:id
+                failureRedirect: "/employee_login",
+                failureFlash: true,
+            })
+        );
+
+        router.get("/logout", this.isLogged, (req, res) => {
+            req.logout(function (err) {
+                if (err) {
+                    return err;
+                }
+                res.redirect("/employee_login");
+            });
+        });
+
+        return router;
+    }
+}
+
+module.exports = AuthRouter;
