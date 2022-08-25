b134512d505ae2d53231a810ff639a756a68d86a
ChloeYeung <106992258+ChloeYeung@users.noreply.github.com>
2022-08-25 13:16:01 +0800

    Service employee - showEmployeeSummary

--- Service/employee_service.js
+++ Service/employee_service.js
@@ -4,31 +4,86 @@ class nodeServiceEmployee {
         this.knex = knex;
     }
 
+
+
+
     init() {
         new Promise((resolve, reject) => {
 
         });
     }
 
-    /* GET /salary/:id */
-    //Working Hours, Hourly Rate, Total Salary
-    showEmployeeSummary(id) {
 
+
+    /* GET /salary/:id */ //Working Hours, Hourly Rate, Total Salary
+    showEmployeeSummary(id) {
+        let command = async function () {
+            // acquire hourly rate
+            let queryHourlyRate = await this.knex
+                .select("hourly_rate")
+                .from("salary")
+                .where("employee_id", id)
+                .then((rows) => {
+                    try {
+                        console.log("Employee Service - hourly rate: " + rows[0].hourly_rate);
+                        return rows[0].hourly_rate;
+                    }
+                    catch {
+                        console.log("Employee Service: queryHourlyRate Error");
+                    }
+                });
+
+            // cal total work hours
+            let queryWorkHours = await this.knex
+                .select("in_time", "out_time")
+                .from("attendance")
+                .where("employee_id", id)
+                .orderBy("id", "asc")
+                .then((rows) => {
+                    try {
+                        let workHoursArray = [];
+                        for (i = 0; i < rows.length; i++) {
+                            const [hours, minutes, seconds] = (rows[i].in_time).split(':');
+                            function convertToSeconds(hours, minutes, seconds) {
+                                return Number(hours) * 60 * 60 + Number(minutes) * 60 + Number(seconds);
+                            }
+                            workHoursArray.push(parseInt((convertToSeconds(hours, minutes, seconds)) / 60 / 60));
+                        }
+                        const initialValue = 0;
+                        const totalWorkHours = workHoursArray.reduce(
+                            (previousValue, currentValue) => previousValue + currentValue,
+                            initialValue
+                        );
+                        console.log(`Employee Service - total working hours: ${totalWorkHours}`);
+                        return totalWorkHours;
+                    }
+                    catch {
+                        console.log("Employee Service queryWorkHours Error");
+                    }
+                })
+        }
+        command();
     };
+
+
 
     /* POST /punchin/:id/:date */
     employeePunchIn(id, date) {
 
     };
 
+
+
     /* POST /punchout/:id/:date */
     employeePunchOut(id, date) {
 
     };
 
+
+
     /* GET /calendar/:id/:date */
     showEmployeeCalendar(id, date) {
-        let query = this.knex
+        let query_salary = this.knex
             .select("employee_id", "in_date", "in_time", "out_time")
             .from("attendance")
             .where("id", id)
@@ -39,11 +94,15 @@ class nodeServiceEmployee {
         })
     };
 
+
+
     /* GET /info/:id */
     //Name, position, id, hourly rate, phone no, address, date of brith, gender
     showEmployeeInfo(id) {
 
     };
+
+
 
     /* PUT /info/:id */
     updateEmployeeInfo(id) {
---
+++ public/calendar_testing.js
@@ -0,0 +1,105 @@
+//For testing//
+
+require("dotenv").config();
+const knex = require("knex")({
+    // CODE HERE
+    client: 'postgresql',
+    connection: {
+        database: "template1",
+        user: "postgres",
+        password: "password",
+        dateStrings: true
+    },
+});
+
+let in_time = "";
+let out_time = "";
+let employee_id = "";
+let inDate = [];
+let outdate = "";
+in_date_result = "";
+
+//below return it as correct date, 19->20, 20->21
+// for (i = 1; i <= 3; i++) {
+//     let query = knex.select("employee_id", "in_date").from("attendance")
+//         .where("employee_id", i)
+//         .orderBy('employee_id', 'asc');
+
+//     query.then((rows) => {
+//         console.log(rows); //!PROBLEM HERE! it return 19 & 20, therefore, I changed it to 20 & 21 below
+//         for (let j = 0; j < rows.length; j++) {
+//             let value = (rows[j].in_date);
+
+//             value = value.toString();
+
+//             const date = value.split(' ', 4).join(' ');
+//             const time = value.split(' ').slice(4).join(' ').split(' ')[0];
+
+//             let [month, day, year] = date.split(' ').slice(1).join(' ').split(' ');
+
+//             const [hours, minutes, seconds] = time.split(':');
+//             var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+//             month = (months.indexOf(month) + 1)
+//             let alteredDate = new Date(Date.UTC(Number(year), Number(month) - 1, Number(day), Number(hours), Number(minutes), Number(seconds)));
+
+//             console.log(alteredDate);
+//         }
+//     }).catch((error) => {
+//         console.log(error);
+//     });
+// }
+
+let command = async function () {
+    // acquire hourly rate
+    let queryHourlyRate = await knex
+        .select("hourly_rate")
+        .from("salary")
+        .where("employee_id", "1")
+        .then((rows) => {
+            try {
+                console.log("Employee Service - hourly rate: " + rows[0].hourly_rate);
+                return rows[0].hourly_rate;
+            }
+            catch {
+                console.log("Employee Service: queryHourlyRate Error");
+            }
+        });
+
+
+    // cal total work hours
+    let queryWorkHours = await knex
+        .select("in_time", "out_time")
+        .from("attendance")
+        .where("employee_id", "1")
+        .orderBy("id", "asc")
+
+        .then((rows) => {
+            try {
+                let workHoursArray = [];
+
+                for (i = 0; i < rows.length; i++) {
+                    const [hours, minutes, seconds] = (rows[i].in_time).split(':');
+                    function convertToSeconds(hours, minutes, seconds) {
+                        return Number(hours) * 60 * 60 + Number(minutes) * 60 + Number(seconds);
+                    }
+                    workHoursArray.push(parseInt((convertToSeconds(hours, minutes, seconds)) / 60 / 60));
+                }
+
+                const initialValue = 0;
+                const totalWorkHours = workHoursArray.reduce(
+                    (previousValue, currentValue) => previousValue + currentValue,
+                    initialValue
+                );
+                console.log(`Employee Service - total working hours: ${totalWorkHours}`);
+                return totalWorkHours;
+
+            }
+            catch {
+                console.log("Employee Service queryWorkHours Error");
+            }
+        })
+}
+command();
+
+
+
